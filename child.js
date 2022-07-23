88e01bcaed8a2b4efd8ab71d2b702698e5e0d388
TechSC <accueil@techsc.fr>
2022-07-24 00:46:34 +0200

    Almost the end of the first video

---
+++ child.js
@@ -0,0 +1,25 @@
+var cp = require('child_process');
+let child = cp.spawn('bluetoothctl', )
+child.stdout.on('data', function (data) {
+    console.log(`${data}`);
+});
+child.stderr.on('data', function (data) {
+    console.log(`${data}`);
+});
+process.stdin.on('data', function (data) {
+    child.stdin.write(data)
+})
+
+child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+//process.on("message", function (message) {
+//    console.log(`Message from main.js: ${message}`);
+//    cp.stdin.write(message)
+//});
+//cp.stdout.on('data', function (data) {
+//    console.log(`Message from main.js: ${data}`)
+//    process.send(data)
+//});
+
