375548f7303796a3d2bf8182759cc7a8e6e1227d
TechSC <accueil@techsc.fr>
2022-07-24 09:25:37 +0200

    Possible breaking change in "terminal dynamique"

--- child.js
+++ child.js
@@ -1,5 +1,5 @@
 var cp = require('child_process');
-let child = cp.spawn('bluetoothctl', )
+let child = cp.spawn('pulseaudio', ["--start"])
 child.stdout.on('data', function (data) {
     console.log(`${data}`);
 });
